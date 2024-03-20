734da24e2f41690b3226500466ac71ffdfb0b4bd
Aldry Rocha <aldryrocha@gmail.com>
2024-03-20 16:52:13 -0300

    :sparkles: feat: component Banner

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Cabecalho from "@/components/cabecalho"
 import Rodape from "@/components/Rodape"
+import Banner from "@/components/Banner"
 
 const Inicio = () => {
     return (
         <>
             <Cabecalho />
+            <Banner imagem='home' />
             <Rodape />
         </>        
     )
