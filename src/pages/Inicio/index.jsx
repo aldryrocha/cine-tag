29e62e99a9a88b8319ba48647928ad74dedd655a
Aldry Rocha <aldryrocha@gmail.com>
2024-03-21 11:37:56 -0300

    :sparkles: feat: Page Favoritos & Component Container

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,6 +1,4 @@
-import React from "react"
-import Cabecalho from "@/components/cabecalho"
-import Rodape from "@/components/Rodape"
+
 import Banner from "@/components/Banner"
 import Titulo from "@/components/Titulo"
 import Card from "@/components/Card"
@@ -11,13 +9,11 @@ import styles from './Inicio.module.css'
 const Inicio = () => {
     return (
         <>
-            <Cabecalho />
             <Banner imagem='home' />
             <Titulo>Um lugar para guardar seus vídeos e filmes!</Titulo>
             <section className={styles.container}>
                 {videos.map((video) => <Card {...video} key={video.id} /> )}
             </section>
-            <Rodape />
         </>        
     )
 }
