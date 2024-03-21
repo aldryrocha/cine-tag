3ad9590daf5e452fb1d1f97f00a32c260a499fc1
Aldry Rocha <aldryrocha@gmail.com>
2024-03-21 10:57:42 -0300

    :lipstick: feat: Estilização CSS do titulo e card e paginaInicial

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -5,13 +5,18 @@ import Banner from "@/components/Banner"
 import Titulo from "@/components/Titulo"
 import Card from "@/components/Card"
 
+import videos from '@/json/db.json'
+import styles from './Inicio.module.css'
+
 const Inicio = () => {
     return (
         <>
             <Cabecalho />
             <Banner imagem='home' />
             <Titulo>Um lugar para guardar seus vídeos e filmes!</Titulo>
-            <Card />
+            <section className={styles.container}>
+                {videos.map((video) => <Card {...video} key={video.id} /> )}
+            </section>
             <Rodape />
         </>        
     )
