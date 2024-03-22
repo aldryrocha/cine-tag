400a89ba7a08f8d55159040545aee7ee769378f2
Aldry Rocha <aldryrocha@gmail.com>
2024-03-22 12:01:13 -0300

    :recycle: refactor: refatorando routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,22 +7,19 @@ import Cabecalho from "@/components/Cabecalho"
 import Rodape from "@/components/Rodape"
 import Container from "@/components/Container"
 import FavoritosProvider from "@/contexts/Favoritos"
+import PaginaBase from "./pages/PaginaBase"
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
-            <Cabecalho />
-            <Container>
-                <FavoritosProvider>
-                    <Routes>
-                        <Route path="/" element={<Inicio />}></Route>
-                        <Route path="/favoritos" element={<Favoritos />}></Route>
-                        <Route path="video/:id" element={<Player />}></Route>
-                        <Route path='*' element={<NaoEncontrada />}></Route>
-                    </Routes>
-                </FavoritosProvider>                
-            </Container>
-            <Rodape />
+            <Routes>
+                <Route path='/' element={<PaginaBase />}>
+                    <Route index element={<Inicio />}></Route>
+                    <Route path="favoritos" element={<Favoritos />}></Route>
+                    <Route path="video/:id" element={<Player />}></Route>
+                    <Route path='*' element={<NaoEncontrada />}></Route>
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
